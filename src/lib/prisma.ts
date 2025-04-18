986a4d91728d343eb20ae05ded6e1354806a945c
Mike <mr.mikesorrentino@gmail.com>
2025-04-18 14:25:49 -0400

    prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,30 @@
+// import { PrismaClient } from '@prisma/client';
+
+// const globalForPrisma = globalThis as unknown as {
+// 	prisma: PrismaClient | undefined;
+// };
+
+// export const prisma =
+// 	globalForPrisma.prisma ??
+// 	new PrismaClient({
+// 		log: ['query'],
+// 	});
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+
+// @ts-nocheck
 import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as {
-	prisma: PrismaClient | undefined;
-};
+let prisma: PrismaClient;
 
-export const prisma =
-	globalForPrisma.prisma ??
-	new PrismaClient({
-		log: ['query'],
-	});
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export default prisma;
